98f5c4e60306dc68f0de5c7e9ad4c041bf3b98d6
Oscar Esquivel <zapata2104@gmail.com>
2025-05-20 20:08:19 -0600

    hola mundo v1.0

---
+++ Python1/hola_mundo.py
@@ -0,0 +1,15 @@
+# Primer comentario - se usa para comentar una solo linea
+'''
+Esto es un comentarios
+multilinea
+que permite informar con mayor carga informativa
+'''
+
+"""
+esto tambien es un 
+comentario multilinea 
+"""
+
+# Instruccion para imprimir en pantalla
+
+print("¡Hola mundo!") # Esta linea sirve para imprimir "¡Hola Mundo!"
